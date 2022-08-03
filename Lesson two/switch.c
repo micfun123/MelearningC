541981d601e600443d3b453f9e768131f096a00d
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-08-03 21:12:51 +0100

    switchers

---
+++ Lesson two/switch.c
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+
+int main(){
+
+    int day;
+
+    printf("Pls enter a day\n");
+    scanf("%d" ,&day);
+    
+    switch (day){
+        case 1:
+            printf("I dont like mondays");
+            break;
+        case 2:
+            printf("tusdays are ok");
+            break;
+        case 3:
+            printf("I love fridays");
+            break;
+
+        
+    }
+
+
+
+}
