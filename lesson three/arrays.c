f0b83eee4dcc461d78a843d25cba236a8a384dbb
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-08-04 18:46:00 +0100

    ok arrrays done

--- lesson three/arrays.c
+++ lesson three/arrays.c
@@ -6,8 +6,10 @@ int othernum[] = {10, 20, 30, 40,50,60,70,80,90,100};
 int main(){ 
    // array inside of main
   int myNumbers[] = {1, 2, 3, 4,5,6,7,8,9,10};
-  printf("%d\n", myNumbers[0]);
-  printf("%d", othernum[7]);
+  printf("%d\n", myNumbers[3]);
+  printf("%d\n", othernum[7]);
+  myNumbers[3] = 6;
+  printf("%d\n", myNumbers[3]);
  
   return 0;
 }
