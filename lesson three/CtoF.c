d149dbdb84790080f8c77b03d8f6ccd77ffd91cc
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-08-04 19:46:20 +0100

    CtoF

---
+++ lesson three/CtoF.c
@@ -0,0 +1,19 @@
+
+
+#include <stdio.h>
+float c;
+float result;
+
+float contof(float temp){
+    float f = (temp * 9/5) + 32;
+    return f;
+    }
+
+int main(){
+
+    printf("Ener a C temp to be turn to F: ");
+    scanf("%f",&c);
+    result = contof(c);
+    printf("%f is the temp in f", result);
+}
+
