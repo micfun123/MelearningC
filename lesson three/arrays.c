ad75a0580efdd9bc92b9b679ca3910dcd57b9b8d
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-08-04 18:31:33 +0100

    simple array

---
+++ lesson three/arrays.c
@@ -0,0 +1,13 @@
+/* Prints a message on the screen */
+#include <stdio.h>
+
+int othernum[] = {10, 20, 30, 40,50,60,70,80,90,100};
+
+int main(){ 
+   // array inside of main
+  int myNumbers[] = {1, 2, 3, 4,5,6,7,8,9,10};
+  printf("%d\n", myNumbers[0]);
+  printf("%d", othernum[7]);
+ 
+  return 0;
+}
