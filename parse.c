39424733ee06292910497f136c6cce8efb660bca
micfun123 <54761507+micfun123@users.noreply.github.com>
2023-03-07 14:40:24 +0000

    parse

--- parse.c
+++ parse.c
@@ -5,19 +5,17 @@ char parser(char myString[],int string_size){
 
     for (size_t i = 0; i < string_size; i++)
     {
-        if (myString[i] == ' ')
-        {
-            myString[i] = '\0';
-            printf("%s\n",myString[i]);
+        if(myString[i] == ' '){
+            myString[i] = '\n';
         }
+        printf("%c",myString[i]);
     }
-    
     
 }
 
 void main(){
 
-    char myString[] = "Hello World!";
+    char myString[] = "Hello World! Hey farther aka spider it works. Am i a good son?";
     parser(myString,sizeof(myString));
 
 }
