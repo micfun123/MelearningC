c9743949f8e7938bf1adc841b375510700be4bbb
micfun123 <54761507+micfun123@users.noreply.github.com>
2023-03-07 15:01:07 +0000

    parse

--- parse.c
+++ parse.c
@@ -1,27 +1,23 @@
 #include <stdio.h>
 #include <string.h>
 
-char newstring[900];
-
-char parser(char myString[],int string_size){
+char parser(const char myString[],int string_size){
 
     for (size_t i = 0; i < string_size; i++)
     {
         if(myString[i] == ' '){
-            newstring[i] = '\n';
+            printf("\n");
         }
-        else{
-            newstring[i] = myString[i];
+        else {
+            printf("%c",myString[i]);
         }
-
-        printf("%c",newstring[i]);
     }
     
 }
 
 void main(){
 
-    char myString[] = "Hello World! Hey farther aka spider it works. Am i a good son?";
+    char myString[] = "Hello World! Hey farther aka spider it works. Am i a good sun?";
     parser(myString,sizeof(myString));
 
 }
