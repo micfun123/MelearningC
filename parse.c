1b99a0ea8603c80329fe15df472159f31032fcdd
micfun123 <54761507+micfun123@users.noreply.github.com>
2023-03-08 21:45:46 +0000

    start and end not working rest is

--- parse.c
+++ parse.c
@@ -2,11 +2,10 @@
 #include <string.h>
 
 char parser(const char myString[]){
-
     for (size_t i = 0; i < strlen(myString); i++)
     {
         if(myString[i] == ' '){
-            if(myString[i-1] == ' '){
+            if(myString[i+1] == ' '){
                 continue;
             }
             else{
@@ -17,12 +16,11 @@ char parser(const char myString[]){
             printf("%c",myString[i]);
         }
     }
-    
 }
 
 void main(){
 
-    char myString[] = "          Hello       World! Hey    farther aka spider            it works. Am i a good sun?";
+    char myString[] = "          Hello       World! Hey    farther aka spider            it works. Am i a good sun?  ";
     parser(myString);
 
 }
