282dbf6333eb941bf24bde4fb571f10a3ea91a84
micfun123 <54761507+micfun123@users.noreply.github.com>
2023-03-09 17:39:39 +0000

    last digit

--- parse.c
+++ parse.c
@@ -1,31 +1,31 @@
-#include <stdio.h>
-#include <string.h>
+    #include <stdio.h>
+    #include <string.h>
 
-char parser(const char myString[]){
-    for (size_t i = 0; i < strlen(myString); i++)
-    {
-        if(myString[i] == ' '){
-            if(myString[i+1] == ' '){
-                continue;
+    char parser(const char myString[]){
+        for (size_t i = 0; i < strlen(myString); i++)
+        {
+            if(myString[i] == ' '){
+                if(myString[i+1] == ' '){
+                    continue;
+                }
+                else if (myString[i+1]== '\0')
+                {
+                break;
+                }
+                
+                else{
+                    printf("\n",myString[i]);
+                }
             }
-            else if (myString[i+1]== '\0')
-            {
-               break;
+            else {
+                printf("%c",myString[i]);
             }
-            
-            else{
-                printf("\n",myString[i]);
-            }
-        }
-        else {
-            printf("%c",myString[i]);
         }
     }
-}
 
-void main(){
-
-    char myString[] = "          Hello       World! Hey    farther aka spider            it works. Am i a good sun?               ";
-    parser(myString);
-
-}
+    int main()
+    {
+        char myString[] = "          Hello       World! Hey        you proud of             me? Please        .       ";
+        parser(myString);
+        return 0;
+    }
