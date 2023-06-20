f9a32f5ccd65a3e0f7f1f6bb7eeefe2e44a75200
Michael Parker <michaelrbparker@gmail.com>
2023-06-20 22:03:48 +0100

    upper.c

--- upper.c
+++ upper.c
@@ -2,34 +2,34 @@
 
 void all_upper(char* str)
 {
-  // array of capital letters
-    printf("%s", str); 
-    char capital[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    // array of capital letters
+    char capital[27] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
     char* p = str;
     int isUpper = 0;
-    while(*p != '\0'){
-        for(int i = 0; i < 26; i++){
-            if(*p == capital[i]){
+    while (*p != '\0') {
+        for (int i = 0; i < 27; i++) {
+            if (*p == capital[i]) {
                 isUpper = 1;
                 break;
             }
         }
-        if(isUpper == 0){
+        if (isUpper == 0) {
             *p = *p - 32;
         }
         isUpper = 0;
         p++;
     }
-    isUpper = 0;
 }
 
 int main()
 {
-  char* str1 = "Gargantua is drinking tea.\n";
-  all_upper(str1);
-  printf(str1); // Should print "GARGANTUA IS DRINKING TEA."
+    char str1[] = "Gargantua is drinking tea.";
+    all_upper(str1);
+    printf("%s\n", str1); // Should print "GARGANTUA IS DRINKING TEA."
 
-  char* str2 = "Tea is drunk by Tea.\n";
-  all_upper(str2);
-  printf(str2); // Should print "TEA IS DRUNK BY TEA."
+    char str2[] = "Tea is drunk by Tea.";
+    all_upper(str2);
+    printf("%s\n", str2); // Should print "TEA IS DRUNK BY TEA."
+
+    return 0;
 }
