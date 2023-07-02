73a78091c5c493b111014a1c86aefa2aee2465f0
Michael Parker <michaelrbparker@gmail.com>
2023-07-02 08:58:59 +0100

    len of all words

---
+++ wordslen.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int len(char* str)
+{
+    int count = 0;
+    while (*str != '\0') {
+        if (*str == ' ') {
+            printf("%d ", count); 
+            count = 0;
+        }
+        else {
+            count++;
+        }
+        str++;
+    }
+    printf("%d\n", count); 
+}
+
+int main()
+{
+    char str1[] = "gargantua is drinking tea.";
+    len(str1);
+    char str2[] = "Tea is drunk by tea.";
+    len(str2);
+    char str3[] = "";
+    len(str3);
+    return 0;
+}
