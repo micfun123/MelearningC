3274e68a10a3205da41121bc57dc81da8233bf56
Michael Parker <michaelrbparker@protonmail.com>
2023-07-09 11:10:52 +0100

    using memory

---
+++ malloc.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+	// This pointer will hold the
+	// base address of the block created
+	int* ptr;
+	int n = 1000, i;
+
+	// Dynamically allocate memory using malloc()
+	ptr = (int*)malloc(n * sizeof(int));
+
+	// Check if the memory has been successfully
+	// allocated by malloc or not
+	if (ptr == NULL) {
+		printf("Memory not allocated.\n");
+		exit(0);
+	}
+	else {
+
+		// Memory has been successfully allocated
+		printf("Memory successfully allocated using malloc.\n");
+
+		// Get the elements of the array
+		for (i = 0; i < n; ++i) {
+			ptr[i] = i + 1; // add the ith element of the array that stored in ptr (memory address)
+		}
+
+		// Print the elements of the array
+		printf("The elements of the array are: ");
+		for (i = 0; i < n; ++i) {
+			printf("%d, ", ptr[i]); // print the ith element of the array that stored in ptr (memory address)
+		}
+	}
+
+	return 0;
+}
