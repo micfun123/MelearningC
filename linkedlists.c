06fd7293de4f441310aa89b22d32cb4731dc5821
Michael Parker <michaelrbparker@protonmail.com>
2023-07-09 13:28:52 +0100

    circual linked lists

--- linkedlists.c
+++ linkedlists.c
@@ -22,13 +22,11 @@ int printList(struct node *n)
 
 int addNode(int data)
 {
-    struct node *new_node = (struct node *)malloc(sizeof(struct node));
-    new_node->data = data;
-    new_node->next = head;
-    head = new_node;
-    return 0;
+    struct node *newNode = (struct node *)malloc(sizeof(struct node));
+    newNode->data = data;
+    newNode->next = head;
+    head = newNode;
 }
-
 
 
 void main(){
