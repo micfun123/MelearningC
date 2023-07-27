280edb098177c7a367790dd6d3845bd4a4a58e3d
micfun123 <54761507+micfun123@users.noreply.github.com>
2023-07-27 09:41:08 +0100

    decoder

--- RLE.c
+++ RLE.c
@@ -16,8 +16,30 @@ void RLE(char* str)
     
 }
 
+void RLE_Decode(char* str)
+{
+    char* p = str;
+    int count = 0;
+    while (*p != '\0') {
+        if (*p >= '0' && *p <= '9') {
+            count = count * 10 + *p - '0';
+        } else {
+            for (int i = 0; i < count; i++) {
+                printf("%c", *p);
+            }
+            count = 0;
+        }
+        p++;
+    }
+}
+
+
 int main(void) {
     char str[] = "aaabbcd";
     RLE(str);
+    printf("\n");
+    char str2[] = "3a2b1c1d";
+    RLE_Decode(str2);
+    
     return 0;
 }
